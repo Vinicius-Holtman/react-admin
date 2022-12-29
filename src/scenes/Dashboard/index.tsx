d3b8bf04dbd91f4544e19ef44f424efa1e2c023a
Vinicius Holtman <viniholtman28@gmail.com>
2022-12-29 16:32:32 -0300

    Implemented component header in dashboard component

--- src/scenes/Dashboard/index.tsx
+++ src/scenes/Dashboard/index.tsx
@@ -3,8 +3,10 @@ import { Header } from "../../components/Header";
 
 export function Dashboard() {
   return (
-    <Box>
-      <Header title="DASHBOARD" subtitle="Welcome to your dashboard!" />
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard!" />
+      </Box>
     </Box>
   )
 }
