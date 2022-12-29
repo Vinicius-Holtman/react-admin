10c1c4a1a53d44700d67f31262d273db6f5be569
Vinicius Holtman <viniholtman28@gmail.com>
2022-12-29 11:30:02 -0300

    Create person hook to toggle mode theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { ColorModeContext, useMode } from "./theme";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { Topbar } from "./scenes/global/Topbar";
+
 function App() {
+  const { theme, colorMode } = useMode()
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
