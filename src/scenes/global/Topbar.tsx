e06958c4b63fde56955929e003298d4e8cb4a69a
Vinicius Holtman <viniholtman28@gmail.com>
2022-12-29 12:00:20 -0300

    Finished top bar layout

--- src/scenes/global/Topbar.tsx
+++ src/scenes/global/Topbar.tsx
@@ -3,7 +3,7 @@ import { Box, IconButton, InputBase, useTheme } from "@mui/material"
 import { useContext } from "react"
 import { ColorModeContext, tokens } from "../../theme"
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
-import DarkModeOutlinedIcon from "@mui/icons-material/NotificationsOutlined"
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined"
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
@@ -19,20 +19,22 @@ export function Topbar() {
       <Box
         display="flex"
         borderRadius="3px"
-        color={colors.primary[400]}
+        sx={{
+          backgroundColor: colors.primary[400]
+        }}
       >
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton>
-          <SearchIcon type="button" sx={{ p: 1 }} />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
         </IconButton>
       </Box>
 
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
           {theme.palette.mode === "dark" ? (
-            <DarkModeOutlinedIcon />
-          ) : (
             <LightModeOutlinedIcon />
+          ) : (
+            <DarkModeOutlinedIcon />
           )}
         </IconButton>
         <IconButton>
