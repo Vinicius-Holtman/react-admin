3f9b01f601186fe210fe79a1d3b4e3555207e768
Vinicius Holtman <viniholtman28@gmail.com>
2022-12-29 16:29:38 -0300

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { Topbar } from "./scenes/global/Topbar";
 import { Sidebar } from "./scenes/global/Sidebar";
-import { Dashboard } from "./scenes/dashboard";
+import { Dashboard } from "./scenes/Dashboard";
 // import { Team } from "./scenes/team";
 // import { Invoices } from "./scenes/invoices";
 // import { Contacts } from "./scenes/contects";
---
+++ src/scenes/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { Header } from "../../components/Header";
+
+export function Dashboard() {
+  return (
+    <Box>
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard!" />
+    </Box>
+  )
+}
